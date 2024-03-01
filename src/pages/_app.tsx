c5bc8d468a8ec4066f43d8ee5c5a92f069c35c94
WajPad <132859629+WajPad@users.noreply.github.com>
2024-03-01 13:19:36 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -44,13 +44,13 @@ const Ogp = () => (
       name="description"
       content="Your music, your stats, your story. Enter a new dimension of music by getting unique insights into your music taste."
     />
-    <meta property="og:title" content="stats.fm" />
+    <meta property="og:title" content="vintagemedia.net" />
     <meta
       property="og:description"
       content="Your music, your stats, your story. Enter a new dimension of music by getting unique insights into your music taste."
     />
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://stats.fm" />
+    <meta property="og:url" content="https://vintagemedia.net" />
     <meta
       property="og:image"
       content="https://next.stats.fm/images/banner.png"
